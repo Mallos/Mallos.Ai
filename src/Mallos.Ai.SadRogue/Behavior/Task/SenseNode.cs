77ae1239544087443a956f601a2aae094f16ee61
Eric Tuvesson <eric.tuvesson@gmail.com>
2019-07-17 22:25:18 +0200

    Add legacy SenseNode and CreateNoiseNode

---
+++ src/Mallos.Ai.SadRogue/Behavior/Task/SenseNode.cs
@@ -0,0 +1,23 @@
+﻿namespace Mallos.Ai.Behavior.Task
+{
+    using System;
+
+    [Obsolete("Waiting on Map to support SenseMap")]
+    [BehaviorCategory(BehaviorCategory.Task)]
+    public class SenseNode : BehaviorTreeNode
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SenseNode"/> class.
+        /// </summary>
+        public SenseNode()
+        {
+            // TODO: Waiting on SadRogue
+        }
+
+        /// <inheritdoc />
+        protected override BehaviorReturnCode Behave(Blackboard blackboard)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
